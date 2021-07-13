bed846ecc7fe62b7665c0173dae5ba373f39745e
avivgu <guetaaviv@gmail.com>
2021-07-13 14:02:37 +0300

    Create ccdsfd.tf

---
+++ src/forests/ccdsfd.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "data_science" {
+  # bucket is not encrypted
+  bucket = "bc-aviv-bla-data-science"
+  acl    = "private"
+  versioning {
+    enabled = var.enabled
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "data_science" {
+  # bucket is not encrypted
+  bucket = "bc-aviv-bla-data-science"
+  acl    = "private"
+  versioning {
+    enabled = var.enabled
+  }
+  force_destroy = true
+}
