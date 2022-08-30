dedebd49cd23b07b490de33ec44276874eb57503
avivgu <guetaaviv@gmail.com>
2022-08-30 11:49:24 +0300

    Update dganit.tf

--- src/dganit.tf
+++ src/dganit.tf
@@ -181,6 +181,35 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "flowbucket" {
   }
 }
 
+resource "aws_security_group" "web-node" {
+  # security group is open to the world in SSH port
+  name        = "${local.resource_prefix.value}-sg"
+  description = "${local.resource_prefix.value} Security Group"
+  vpc_id      = aws_vpc.web_vpc.id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  depends_on = [aws_vpc.web_vpc]
+}
 
 
 output "ec2_public_dns" {
